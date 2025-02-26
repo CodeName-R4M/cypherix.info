41dae0b813eb4924ab5bba553124fc36b0cf0d76
Sriram N <sriramn20060126@gmail.com>
2025-02-26 15:31:56 +0530

    added countdown

--- assets/js/Script.js
+++ assets/js/Script.js
@@ -7,7 +7,13 @@ function toggleMenu() {
     console.error("Menu element not found! Check if '.nav-links.mobile-menu' exists in HTML.");
   }
 }
-
+jQuery('#clock').countdown('2025/2/28',function(event){
+  var $this=jQuery(this).html(event.strftime(''
+  +'<div class="time-entry days"><span>%-D</span> <b>:</b> Days</div> '
+  +'<div class="time-entry hours"><span>%H</span> <b>:</b> Hours</div> '
+  +'<div class="time-entry minutes"><span>%M</span> <b>:</b> Minutes</div> '
+  +'<div class="time-entry seconds"><span>%S</span> Seconds</div> '));
+});
 // document.querySelectorAll('.event').forEach(event => {
 //   event.addEventListener('click', function() {
 //       this.classList.toggle('active');
